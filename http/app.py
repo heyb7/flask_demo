fb10d40c5efca4bb2c341fb0031175a51ecdf8ae
hyb7 <heyanbing7@gmail.com>
2019-04-11 00:50:46 +0800

    add new code

--- http/app.py
+++ http/app.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from flask import Flask
 from flask import request
@@ -7,16 +8,23 @@ from flask import url_for
 from flask import abort
 from flask import make_response
 from flask import jsonify
-
+from flask import session
 
 app = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY', 'secret string')
 
 @app.route('/hello')
 def hello():
     name = request.args.get('name')
     if name is None:
         name = request.cookies.get('name', 'Flask')
-    return '<h1>Hello, %s!</h1>' % name
+        response = '<h1>Hello, %s!</h1>' % name
+        if 'logged_in' in session:
+            response += '[Authenticated]'
+        else:
+            response += '[Not Authenticated]'
+    return response
 
 @app.route('/goback/<int:year>')
 def go_back(year):
@@ -107,3 +115,39 @@ def set_cookie(name):
     response = make_response(redirect(url_for('hello')))
     response.set_cookie('name', name)
     return response
+
+@app.route('/admin')
+def admin():
+    if 'logged_in' not in session:
+        abort(403)
+    return 'Welcome to admin page'
+
+@app.route('/login')
+def login():
+    session['logged_in'] = True # 写入session，并使用secret_key加密
+    return redirect(url_for('hello'))
+
+@app.route('/logout')
+def logout():
+    if 'logged_in' in session:
+        session.pop('logged_in')
+    return redirect(url_for('hello'))
+
+@app.route('/foo')
+def foo():
+    return '<h1>Foo page</h1> <a href="%s">Do something</a>' % url_for('do_something', next=request.full_path)
+
+@app.route('/bar')
+def bar():
+    return '<h1>Bar page</h1> <a href="%s">Do something</a>' % url_for('do_something', next=request.full_path)
+
+@app.route('/do_something')
+def do_something():
+    return redirect_back()
+
+
+def redirect_back(default='hello', **kwargs):
+    for target in request.args.get('next'), request.referrer:
+        if target:
+            return redirect(target)
+    return redirect(url_for(default, **kwargs))
