89494aa56b3c921105a39b737355bbac8a0e16e3
heyb <heyb@test.com>
2019-04-25 15:40:39 +0800

    add form

--- form/app.py
+++ form/app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template, flash, url_for, redirect, request
-from forms import LoginForm, FortyTwoForm, UploadForm
+from flask import Flask, render_template, flash, url_for, redirect, request, session, \
+     send_from_directory
+from forms import LoginForm, FortyTwoForm, UploadForm, RichTextForm
+from flask_ckeditor import CKEditor, upload_success, upload_fail
+
+import os
+import uuid
 
 app = Flask(__name__)
 
 app.secret_key = 'secret string'
+
+# 自定义config
+app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')
+
+# flask config
+# set request body's max length
+app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024 # 3Mb
+
+app.config['CKEDITOR_SERVE_LOCAL'] = True
+app.config['CKEDITOR_FILE_UPLOADER'] = 'upload_for_ckeditor'
+
+
+ckeditor = CKEditor(app)
 
 @app.route('/')
 def index():
@@ -33,11 +51,55 @@ def custome_validator():
     return render_template('custom_validator.html', form=form)
 
 
+@app.route('/uploads/<path:filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+
+@app.route('/uploaded-images')
+def show_images():
+    return render_template('uploaded.html')
+
+
+def random_filename(filename):
+    ext = os.path.splitext(filename)[1]
+    new_filename = uuid.uuid4().hex + ext
+    return new_filename
+
+
 @app.route('/upload', methods=['GET','POST'])
 def upload():
     form = UploadForm()
 
     if form.validate_on_submit():
-        pass
+        f = form.photo.data
+        filename = random_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_PATH'],filename))
+        flash('Upload success')
+        session['filenames'] = [filename]
+        return redirect(url_for('show_images'))
 
     return render_template('upload.html', form=form)
+
+
+@app.route('/ckeditor', methods=['GET', 'POST'])
+def integrate_ckeditor():
+    form = RichTextForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        flash('Your post is published')
+        return render_template('post.html', title=title, body=body)
+
+    return render_template('ckeditor.html', form=form)    
+
+
+@app.route('/upload-ck', methods=['POST'])
+def upload_for_ckeditor():
+    f = request.files.get('upload')
+    if extension not in ['jpg', 'gif', 'png', 'jpeg']:
+        return upload_fail('Image only')
+    
+    f.save(os.path.join(app.config['UPLOAD_PATH'], f.filename))
+    url = url_for('get_file', filename=f.filename)
+    return upload_success(url, f.filename)
--- form/forms.py
+++ form/forms.py
@@ -4,6 +4,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Integ
 from wtforms.validators import DataRequired, Length, ValidationError
 
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask_ckeditor import CKEditorField
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired()])
@@ -26,3 +27,10 @@ class FortyTwoForm(FlaskForm):
 class UploadForm(FlaskForm):
     photo = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['jpg','jpeg','png','gif'])])
     submit = SubmitField()
+
+
+# 富文本编辑器表单
+class RichTextForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(1,50)])
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField('Publish')
